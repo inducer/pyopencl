76630b4b19df0d60520b6e483ed89544af093729
Andreas Kloeckner <inform@tiker.net>
2010-06-25 10:12:09 -0400

    Tweak make_constants.py so that it runs with CL 1.0.

--- doc/make_constants.py
+++ doc/make_constants.py
@@ -3,7 +3,6 @@ import pyopencl as cl
 devi = cl.device_info
 ctxi = cl.context_info
 ctxp = cl.context_properties
-gl_ci = cl.gl_context_info
 fpc = cl.device_fp_config
 cho = cl.channel_order
 wgi = cl.kernel_work_group_info
@@ -32,76 +31,79 @@ def get_extra_lines(tup):
         yield "    .. versionadded:: %s" % pyopencl_ver
         yield ""
 
-
 const_ext_lookup = {
         devi: {
-            devi.PREFERRED_VECTOR_WIDTH_HALF: ("CL_1.1", "0.92"),
-            devi.HOST_UNIFIED_MEMORY: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_CHAR: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_SHORT: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_INT: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_LONG: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_FLOAT: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_DOUBLE: ("CL_1.1", "0.92"),
-            devi.NATIVE_VECTOR_WIDTH_HALF: ("CL_1.1", "0.92"),
-            devi.OPENCL_C_VERSION: ("CL_1.1", "0.92"),
+            getattr(devi, "PREFERRED_VECTOR_WIDTH_HALF", None): ("CL_1.1", "0.92"),
+            getattr(devi, "HOST_UNIFIED_MEMORY", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_CHAR", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_SHORT", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_INT", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_LONG", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_FLOAT", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_DOUBLE", None): ("CL_1.1", "0.92"),
+            getattr(devi, "NATIVE_VECTOR_WIDTH_HALF", None): ("CL_1.1", "0.92"),
+            getattr(devi, "OPENCL_C_VERSION", None): ("CL_1.1", "0.92"),
             },
 
         ctxp: {
-            ctxp.GL_CONTEXT_KHR: ("cl_khr_gl_sharing", "0.92"),
-            ctxp.EGL_DISPLAY_KHR: ("cl_khr_gl_sharing", "0.92"),
-            ctxp.GLX_DISPLAY_KHR: ("cl_khr_gl_sharing", "0.92"),
-            ctxp.WGL_HDC_KHR: ("cl_khr_gl_sharing", "0.92"),
-            ctxp.CGL_SHAREGROUP_KHR: ("cl_khr_gl_sharing", "0.92"),
-            },
-
-        gl_ci: {
-            gl_ci.CURRENT_DEVICE_FOR_GL_CONTEXT_KHR:
-            ("cl_khr_gl_sharing", "0.92"),
-
-            gl_ci.DEVICES_FOR_GL_CONTEXT_KHR:
-            ("cl_khr_gl_sharing", "0.92"),
+            getattr(ctxp, "GL_CONTEXT_KHR", None): ("cl_khr_gl_sharing", "0.92"),
+            getattr(ctxp, "EGL_DISPLAY_KHR", None): ("cl_khr_gl_sharing", "0.92"),
+            getattr(ctxp, "GLX_DISPLAY_KHR", None): ("cl_khr_gl_sharing", "0.92"),
+            getattr(ctxp, "WGL_HDC_KHR", None): ("cl_khr_gl_sharing", "0.92"),
+            getattr(ctxp, "CGL_SHAREGROUP_KHR", None): ("cl_khr_gl_sharing", "0.92"),
             },
 
         fpc: {
-            fpc.SOFT_FLOAT: ("CL_1.1", "0.92"),
+            getattr(fpc, "SOFT_FLOAT", None): ("CL_1.1", "0.92"),
             },
 
         ctxi: {
-            ctxi.NUM_DEVICES: ("CL_1.1", "0.92"),
+            getattr(ctxi, "NUM_DEVICES", None): ("CL_1.1", "0.92"),
             },
 
         cho: {
-            cho.Rx: ("CL_1.1", "0.92"),
-            cho.RGx: ("CL_1.1", "0.92"),
-            cho.RGBx: ("CL_1.1", "0.92"),
+            getattr(cho, "Rx", None): ("CL_1.1", "0.92"),
+            getattr(cho, "RGx", None): ("CL_1.1", "0.92"),
+            getattr(cho, "RGBx", None): ("CL_1.1", "0.92"),
             },
 
         wgi: {
-            wgi.PREFERRED_WORK_GROUP_SIZE_MULTIPLE: ("CL_1.1", "0.92"),
-            wgi.PRIVATE_MEM_SIZE: ("CL_1.1", "0.92"),
+            getattr(wgi, "PREFERRED_WORK_GROUP_SIZE_MULTIPLE", None): ("CL_1.1", "0.92"),
+            getattr(wgi, "PRIVATE_MEM_SIZE", None): ("CL_1.1", "0.92"),
             },
 
         iam: {
-            iam.MIRRORED_REPEAT: ("CL_1.1", "0.92"),
+            getattr(iam, "MIRRORED_REPEAT", None): ("CL_1.1", "0.92"),
             },
 
         evi: {
-            evi.CONTEXT: ("CL_1.1", "0.92"),
+            getattr(evi, "CONTEXT", None): ("CL_1.1", "0.92"),
             },
 
         memi: {
-            memi.ASSOCIATED_MEMOBJECT: ("CL_1.1", "0.92"),
-            memi.OFFSET: ("CL_1.1", "0.92"),
+            getattr(memi, "ASSOCIATED_MEMOBJECT", None): ("CL_1.1", "0.92"),
+            getattr(memi, "OFFSET", None): ("CL_1.1", "0.92"),
             },
 
         ctype: {
-            ctype.READ_BUFFER_RECT: ("CL_1.1", "0.92"),
-            ctype.WRITE_BUFFER_RECT: ("CL_1.1", "0.92"),
-            ctype.COPY_BUFFER_RECT: ("CL_1.1", "0.92"),
-            ctype.USER: ("CL_1.1", "0.92"),
+            getattr(ctype, "READ_BUFFER_RECT", None): ("CL_1.1", "0.92"),
+            getattr(ctype, "WRITE_BUFFER_RECT", None): ("CL_1.1", "0.92"),
+            getattr(ctype, "COPY_BUFFER_RECT", None): ("CL_1.1", "0.92"),
+            getattr(ctype, "USER", None): ("CL_1.1", "0.92"),
             },
         }
+try:
+    gl_ci = cl.gl_context_info
+except AttributeError:
+    pass
+else:
+    const_ext_lookup[gl_ci] = {
+            getattr(gl_ci, "CURRENT_DEVICE_FOR_GL_CONTEXT_KHR", None):
+            ("cl_khr_gl_sharing", "0.92"),
+
+            getattr(gl_ci, "DEVICES_FOR_GL_CONTEXT_KHR", None):
+            ("cl_khr_gl_sharing", "0.92"),
+            }
 
 cls_ext_lookup = {
         #cl.buffer_create_type: ("CL_1.1", "0.92"),
