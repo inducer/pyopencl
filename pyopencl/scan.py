1ee0e671127212cb60d647f31f85989e9dc3d3b9
Andreas Kloeckner <inform@tiker.net>
2012-12-17 23:35:49 -0500

    Scan docs: Provide usage example for generic scan.

--- pyopencl/scan.py
+++ pyopencl/scan.py
@@ -1019,6 +1019,20 @@ class _GenericScanKernelBase(object):
 class GenericScanKernel(_GenericScanKernelBase):
     """Generates and executes code that performs prefix sums ("scans") on
     arbitrary types, with many possible tweaks.
+
+    Usage example::
+
+        from pyopencl.scan import GenericScanKernel
+        knl = GenericScanKernel(
+                context, np.int32,
+                arguments="__global int *ary",
+                input_expr="ary[i]",
+                scan_expr="a+b", neutral="0",
+                output_statement="ary[i+1] = item;")
+
+        a = cl.array.arange(queue, 10000, dtype=np.int32)
+        scan_kernel(a, queue=queue)
+
     """
 
     def finish_setup(self):
