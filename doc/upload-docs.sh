7ed6f3111be023bb8221f9860872bdd769c1c9d6
Andreas Kloeckner <inform@tiker.net>
2013-02-12 00:09:02 -0600

    Update doc upload script.

--- doc/upload-docs.sh
+++ doc/upload-docs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 cp build/html/runtime.html build/html/reference.html
-rsync --progress --verbose --archive --delete build/html/* buster:doc/pyopencl
+rsync --progress --verbose --archive --delete build/html/* doc-upload:doc/pyopencl
