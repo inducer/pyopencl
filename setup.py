fe5b8fd3f50bda19e9a286bf2f8f619d8b5f2768
Andreas Kloeckner <inform@tiker.net>
2015-08-23 14:00:57 -0500

    Add -mmacosx-version-min=10.7 to OSX build options (suggested by Gregor Thalhammer)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def get_config_schema():
 
         default_libs = []
         default_cxxflags = default_cxxflags + [
-                '-stdlib=libc++',
+                '-stdlib=libc++', '-mmacosx-version-min=10.7',
                 '-arch', 'i386', '-arch', 'x86_64'
                 ]
 
