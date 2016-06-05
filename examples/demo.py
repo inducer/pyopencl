54194801e3fd8e574c91cf45fdf99ed52ff1ba5b
Andreas Kloeckner <inform@tiker.net>
2016-06-05 01:55:47 -0500

    Demo formatting

--- examples/demo.py
+++ examples/demo.py
@@ -16,7 +16,9 @@ a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
 b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 
 prg = cl.Program(ctx, """
-__kernel void sum(__global const float *a_g, __global const float *b_g, __global float *res_g) {
+__kernel void sum(
+    __global const float *a_g, __global const float *b_g, __global float *res_g)
+{
   int gid = get_global_id(0);
   res_g[gid] = a_g[gid] + b_g[gid];
 }
