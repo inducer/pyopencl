21b471a76d62bf79d4765872a2209de406ed46b1
Shane-J-Latham <shane.latham@anu.edu.au>
2016-06-14 09:30:37 +1000

    Build error fix for when CL_USE_SHIPPED_EXT=False and CL/cl_ext.h doesn't define cl_device_topology_amd struct.

--- src/c_wrapper/pyopencl_ext.h
+++ src/c_wrapper/pyopencl_ext.h
@@ -16,6 +16,16 @@
 #include <CL/cl.h>
 #include <CL/cl_ext.h>
 
+#ifndef CL_DEVICE_TOPOLOGY_TYPE_PCIE_AMD
+#define CL_DEVICE_TOPOLOGY_TYPE_PCIE_AMD        1
+
+typedef union
+{
+    struct { cl_uint type; cl_uint data[5]; } raw;
+    struct { cl_uint type; cl_char unused[17]; cl_char bus; cl_char device; cl_char function; } pcie;
+} cl_device_topology_amd;
+#endif
+
 #endif
 
 #endif
