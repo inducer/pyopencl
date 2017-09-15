4b90d140ef31b340db18afa6afbef71023ed7b27
Andreas Kloeckner <inform@tiker.net>
2017-09-14 20:26:23 -0500

    Un-destroy examples/demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -12,11 +12,8 @@ ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 mf = cl.mem_flags
-a_g = cl.Buffer(ctx, mf.READ_WRITE, a_np.nbytes)
-b_g = cl.Buffer(ctx, mf.READ_WRITE, b_np.nbytes)
-
-cl.enqueue_copy(queue, a_g, a_np)
-cl.enqueue_copy(queue, b_g, b_np)
+a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
+b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 
 prg = cl.Program(ctx, """
 __kernel void sum(
@@ -28,7 +25,7 @@ __kernel void sum(
 """).build()
 
 res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
-prg.sum(queue, (500, 10), (16, 16), a_g, b_g, res_g, g_times_l=True)
+prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
 
 res_np = np.empty_like(a_np)
 cl.enqueue_copy(queue, res_np, res_g)
