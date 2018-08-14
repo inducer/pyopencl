5ebbee62c9f073536e224288676a13bbfef8b16c
Andreas Kloeckner <inform@tiker.net>
2018-08-14 12:57:14 -0500

    Fix Program.from_int_ptr

--- pyopencl/__init__.py
+++ pyopencl/__init__.py
@@ -357,7 +357,7 @@ class Program(object):
     int_ptr = property(int_ptr, doc=_cl._Program.int_ptr.__doc__)
 
     def from_int_ptr(int_ptr_value, retain=True):
-        return Program(_cl._Program.from_int_ptr(int_ptr_value), retain)
+        return Program(_cl._Program.from_int_ptr(int_ptr_value, retain))
     from_int_ptr.__doc__ = _cl._Program.from_int_ptr.__doc__
     from_int_ptr = staticmethod(from_int_ptr)
 
