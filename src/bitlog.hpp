62b26adcafd17c8d77824eb63f7f0267024e6e15
Andreas Kloeckner <inform@tiker.net>
2018-11-01 16:20:17 -0500

    Add license headers to C++ files

--- src/bitlog.hpp
+++ src/bitlog.hpp
@@ -1,4 +1,29 @@
 // Base-2 logarithm bithack.
+//
+// Copyright (C) 2009 Andreas Kloeckner
+// Copyright (C) Sean Eron Anderson (in the public domain)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 
 #ifndef _AFJDFJSDFSD_PYOPENCL_HEADER_SEEN_BITLOG_HPP
 #define _AFJDFJSDFSD_PYOPENCL_HEADER_SEEN_BITLOG_HPP
@@ -10,13 +35,15 @@
 
 namespace pyopencl
 {
+  /* from http://graphics.stanford.edu/~seander/bithacks.html */
+
   extern const char log_table_8[];
 
   inline unsigned bitlog2_16(uint16_t v)
   {
     if (unsigned long t = v >> 8)
       return 8+log_table_8[t];
-    else 
+    else
       return log_table_8[v];
   }
 
@@ -24,7 +51,7 @@ namespace pyopencl
   {
     if (uint16_t t = v >> 16)
       return 16+bitlog2_16(t);
-    else 
+    else
       return bitlog2_16(v);
   }
 
@@ -33,7 +60,7 @@ namespace pyopencl
 #if (ULONG_MAX != 4294967295)
     if (uint32_t t = v >> 32)
       return 32+bitlog2_32(t);
-    else 
+    else
 #endif
       return bitlog2_32(v);
   }
