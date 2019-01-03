e9b66cd3d669973b3fb0273a8bd23c729553eeea
Andreas Kloeckner <inform@tiker.net>
2019-01-03 10:38:58 -0500

    Allow setup_requires numpy, use pybind11 setup widgetry from aksetup

--- setup.py
+++ setup.py
@@ -31,89 +31,12 @@ THE SOFTWARE.
 
 import sys
 from os.path import exists
-import setuptools
-from setuptools.command.build_ext import build_ext
-
-
-# {{{ boilerplate from https://github.com/pybind/python_example/blob/2ed5a68759cd6ff5d2e5992a91f08616ef457b5c/setup.py  # noqa
-
-class get_pybind_include(object):  # noqa: N801
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False) as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        fname = f.name
-    try:
-        compiler.compile([fname], extra_postargs=[flagname])
-    except setuptools.distutils.errors.CompileError:
-        return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct in ['unix', 'mingw32']:
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = ext.extra_compile_args + opts
-        build_ext.build_extensions(self)
-
-# }}}
 
 
 def get_config_schema():
-    from aksetup_helper import ConfigSchema, Option, \
-            IncludeDir, LibraryDir, Libraries, \
-            Switch, StringListOption
+    from aksetup_helper import (ConfigSchema, Option,
+            IncludeDir, LibraryDir, Libraries,
+            Switch, StringListOption)
 
     default_cxxflags = [
             # Required for pybind11:
@@ -184,7 +107,8 @@ SEPARATOR = "-"*75
 def main():
     from setuptools import find_packages
     from aksetup_helper import (hack_distutils, get_config, setup,
-            check_pybind11, check_git_submodules, NumpyExtension)
+            check_pybind11, check_git_submodules, ExtensionUsingNumpy,
+            get_pybind_include, PybindBuildExtCommand)
     check_pybind11()
     check_git_submodules()
 
@@ -299,7 +223,7 @@ def main():
             packages=find_packages(),
 
             ext_modules=[
-                NumpyExtension("pyopencl._cl",
+                ExtensionUsingNumpy("pyopencl._cl",
                     [
                         "src/wrap_constants.cpp",
                         "src/wrap_cl.cpp",
@@ -345,7 +269,7 @@ def main():
                         ]
                     },
 
-            cmdclass={'build_ext': BuildExt},
+            cmdclass={'build_ext': PybindBuildExtCommand},
             zip_safe=False)
 
 
