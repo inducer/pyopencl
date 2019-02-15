54f0ce11643ed3af6cfc72798db7b33835836ef8
Isuru Fernando <isuruf@gmail.com>
2019-02-14 18:28:24 -0600

    Fix commit

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -18,8 +18,8 @@ git clone --branch v2.2.12 https://github.com/OCL-dev/ocl-icd
 cd ocl-icd
 curl -L -O https://raw.githubusercontent.com/conda-forge/ocl-icd-feedstock/master/recipe/install-headers.patch
 git apply install-headers.patch
-curl -L -O https://github.com/isuruf/ocl-icd/commit/c28a6f39634d73c844d70b12120b38c40b17aecc.patch
-git apply c28a6f39634d73c844d70b12120b38c40b17aecc.patch
+curl -L -O https://github.com/isuruf/ocl-icd/commit/76fab891c277886ef88af73c57328f8a47bdb6a4.patch
+git apply 76fab891c277886ef88af73c57328f8a47bdb6a4.patch
 autoreconf -i
 chmod +x configure
 ./configure --prefix=/usr
