dc324b109ef2bbf45244699df2448a9b8fe2e660
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-06-04 00:35:43 -0500

    drop py2

--- setup.py
+++ setup.py
@@ -209,8 +209,6 @@ def main():
                 'Natural Language :: English',
                 'Programming Language :: C++',
                 'Programming Language :: Python',
-                'Programming Language :: Python :: 2',
-                'Programming Language :: Python :: 2.7',
                 'Programming Language :: Python :: 3',
                 'Programming Language :: Python :: 3.2',
                 'Programming Language :: Python :: 3.3',
