859af50c4f14451c17bdb6af84d3a3cc7a24bca7
Andreas Kloeckner <inform@tiker.net>
2020-11-18 12:45:18 -0600

    Improve no-queue printed representation: add shape, dtype

--- pyopencl/array.py
+++ pyopencl/array.py
@@ -799,14 +799,16 @@ class Array:
 
     def __str__(self):
         if self.queue is None:
-            return (f"<cl.Array without queue, call with_queue() to print>")
+            return (f"<cl.Array {self.shape} of {self.dtype} "
+                    "without queue, call with_queue()>")
 
         return str(self.get())
 
     def __repr__(self):
         if self.queue is None:
-            return (f"<cl.Array at {id(self):x} without queue, "
-                    "call with_queue() to print>")
+            return (f"<cl.Array {self.shape} of {self.dtype} "
+                    f"at {id(self):x} without queue, "
+                    "call with_queue()>")
 
         result = repr(self.get())
         if result[:5] == "array":
