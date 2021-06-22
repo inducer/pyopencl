f9fa31522d7015ec67c9a0733a0049024955c8ed
Xiao Liang <yxliang01@users.noreply.github.com>
2021-06-22 20:22:33 +0800

    Update and rename build-wheels.sh to prepare-build-wheels.sh

---
+++ scripts/prepare-build-wheels.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -e -x
+
+function start_spinner {
+    if [ -n "$SPINNER_PID" ]; then
+        return
+    fi
+
+    >&2 echo "Building libraries..."
+    # Start a process that runs as a keep-alive
+    # to avoid travis quitting if there is no output
+    (while true; do
+        sleep 60
+        >&2 echo "Still building..."
+    done) &
+    SPINNER_PID=$!
+    disown
+}
+
+function stop_spinner {
+    if [ ! -n "$SPINNER_PID" ]; then
+        return
+    fi
+
+    kill $SPINNER_PID
+    unset SPINNER_PID
+
+    >&2 echo "Building libraries finished."
+}
+
+#start_spinner
+
+if [[ ! -d ~/deps ]]; then
+
+    # dependencies need to be setup
+
+    mkdir -p ~/deps
+    pushd ~/deps
+
+    curl https://tiker.net/tmp/.tmux.conf
+    yum install -y git yum openssl-devel ruby
+
+    git clone --branch v2.3.0 https://github.com/OCL-dev/ocl-icd
+    cd ocl-icd
+    curl -L -O https://raw.githubusercontent.com/conda-forge/ocl-icd-feedstock/e2c03e3ddb1ff86630ccf80dc7b87a81640025ea/recipe/install-headers.patch
+    git apply install-headers.patch
+    curl -L -O https://github.com/isuruf/ocl-icd/commit/3862386b51930f95d9ad1089f7157a98165d5a6b.patch
+    git apply 3862386b51930f95d9ad1089f7157a98165d5a6b.patch
+    autoreconf -i
+    chmod +x configure
+    ./configure --prefix=/usr
+    make -j$(nproc)
+    make install
+    
+    popd
+fi
+
+# Compile wheels
+PYTHON_VERSION=$(python -c 'import platform; print(platform.python_version())')
+NUMPY_VERSION_SPEC='=='
+if [[ "${PYTHON_VERSION}" == '3.6'* ]]; then
+    NUMPY_VERSION_SPEC="${NUMPY_VERSION_SPEC}1.11.3"
+elif [[ "${PYTHON_VERSION}" == '3.7'* ]]; then
+    NUMPY_VERSION_SPEC="${NUMPY_VERSION_SPEC}1.14.5"
+elif [[ "${PYTHON_VERSION}" == '3.8'* ]]; then
+    NUMPY_VERSION_SPEC="${NUMPY_VERSION_SPEC}1.17.3"
+elif [[ "${PYTHON_VERSION}" == '3.9'* ]]; then
+    NUMPY_VERSION_SPEC="${NUMPY_VERSION_SPEC}1.19.5"
+else
+    # Unknown python version, let it unpinned instead
+    NUMPY_VERSION_SPEC=''
+fi
+# TODO: declear dependencies except numpy as build-time dependency as per PEP518
+# Build with the oldest numpy available to be compatible with newer ones
+pip install "numpy${NUMPY_VERSION_SPEC}" pybind11 mako
+
+# For bundling license files
+pip install delocate
+
+#stop_spinner
