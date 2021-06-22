bbe01daf9144a3d8db8c516124598fe6051b9847
Xiao Liang <yxliang01@users.noreply.github.com>
2021-06-22 20:25:02 +0800

    Update fix-wheel.py

--- scripts/fix-wheel.py
+++ scripts/fix-wheel.py
@@ -5,20 +5,18 @@ from glob import glob
 
 from delocate import wheeltools
 
-def add_library(paths):
-    wheel_fnames = glob('/io/wheelhouse/pyopencl*.whl')
-    for fname in wheel_fnames:
-        print('Processing', fname)
-        with wheeltools.InWheel(fname, fname):
-            libs_dir = os.path.join('pyopencl', '.libs')
-            os.makedirs(libs_dir, exist_ok=True)
-            for lib_path in paths:
-                shutil.copy2(lib_path, libs_dir)
+def add_library(fname, paths):
+    print('Processing', fname)
+    with wheeltools.InWheel(fname, fname):
+        libs_dir = os.path.join('pyopencl', '.libs')
+        os.makedirs(libs_dir, exist_ok=True)
+        for lib_path in paths:
+            shutil.copy2(lib_path, libs_dir)
 
 def main():
-    args = list(sys.argv)
-    args.pop(0)
-    add_library(args)
+    fname = sys.argv[1]
+    paths = sys.argv[2:]
+    add_library(fname, paths)
 
 if __name__ == '__main__':
     main()
