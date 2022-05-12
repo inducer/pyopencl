75aa843c73186963f2c23298acd4f8e0e2072eb8
Grzegorz Bokota <bokota+github@gmail.com>
2022-05-12 23:47:22 +0200

    Update scripts/build-ocl-windows.sh
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- scripts/build-ocl-windows.sh
+++ scripts/build-ocl-windows.sh
@@ -10,7 +10,7 @@ git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-Headers
 cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build 
 cmake --build ./OpenCL-Headers/build --target install
 
-# if someone would like to try to create win32 wheels bellow lines may be usefull
+# if someone would like to try to create win32 wheels bellow lines may be useful
 # cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -DOPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build 
 # cmake --build ./OpenCL-ICD-Loader/build --target install --config Release
 
