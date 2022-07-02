0b41d03cb5f22fc0cd987c8e2097fc2d7a679df4
Andreas Kloeckner <inform@tiker.net>
2022-07-02 00:07:27 -0500

    Add SVM._ptr_as_int()

--- src/wrap_cl_part_2.cpp
+++ src/wrap_cl_part_2.cpp
@@ -299,6 +299,7 @@ void pyopencl_expose_part_2(py::module &m)
     typedef svm_arg_wrapper cls;
     py::class_<cls>(m, "SVM", py::dynamic_attr())
       .def(py::init<py::object>())
+      .def("_ptr_as_int", [](cls &self) { return (intptr_t) self.ptr(); })
       .def("_size", &cls::size)
       ;
   }
