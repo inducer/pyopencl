bcccfe4689b6fcc413c7e6ff9264fad536eac472
Isuru Fernando <isuruf@gmail.com>
2022-10-13 21:01:18 -0500

    Check PYOPENCL_HOME even if /etc/OpenCL/vendors do not exist

--- scripts/build-ocl.sh
+++ scripts/build-ocl.sh
@@ -10,8 +10,8 @@ git clone --branch v2.3.1 https://github.com/OCL-dev/ocl-icd
 cd ocl-icd
 curl -L -O https://raw.githubusercontent.com/conda-forge/ocl-icd-feedstock/e2c03e3ddb1ff86630ccf80dc7b87a81640025ea/recipe/install-headers.patch
 git apply install-headers.patch
-curl -L -O https://github.com/isuruf/ocl-icd/commit/3862386b51930f95d9ad1089f7157a98165d5a6b.patch
-git apply 3862386b51930f95d9ad1089f7157a98165d5a6b.patch
+curl -L -O https://github.com/isuruf/ocl-icd/commit/307f2267100a2d1383f0c4a77344b127c0857588.patch
+git apply 307f2267100a2d1383f0c4a77344b127c0857588.patch
 autoreconf -i
 chmod +x configure
 ./configure --prefix=/usr
