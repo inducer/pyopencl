acc3b470731e8435da01b43d7b9db6910045bf93
Andreas Kloeckner <inform@tiker.net>
2022-11-25 12:51:29 -0600

    Shape comp: comment phrasing, convert to array only once

--- pyopencl/array.py
+++ pyopencl/array.py
@@ -612,17 +612,18 @@ class Array:
 
                 shape = (shape,)
 
-            # Previously the size was computed as
+            shape_array = np.array(shape)
+
+            # Previously, the size was computed as
             #   "size = 1; size *= dim for dim in shape"
-            # However it can fail when using data types,
-            # eg numpy.uint64(1) * 2  returns 2.0 !
-            if np.any(np.array(shape) < 0):
+            # However this can fail when using certain data types,
+            # eg numpy.uint64(1) * 2 returns 2.0 !
+            if np.any(shape_array < 0):
                 raise ValueError(f"negative dimensions are not allowed: {shape}")
             if np.any([np.array([s]).dtype.kind not in ["u", "i"] for s in shape]):
                 raise ValueError(
-                    "Invalid shape %s ; dimensions, must be integer" % (str(shape))
-                )
-            size = np.prod(shape, dtype=np.uint64).item()
+                    "Invalid shape %s ; dimensions, must be integer" % (str(shape)))
+            size = np.prod(shape_array, dtype=np.uint64).item()
 
             if strides is None:
                 if order in "cC":
