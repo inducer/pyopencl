4d31ee710a703ef129c6d53b163b7c3df594fa52
Isuru Fernando <isuruf@gmail.com>
2022-12-09 11:21:40 -0600

    Enable extended alignment for complex types with a define

--- pyopencl/cl/pyopencl-complex.h
+++ pyopencl/cl/pyopencl-complex.h
@@ -274,11 +274,19 @@
     } \
   } \
 
+#if PYOPENCL_COMPLEX_ENABLE_EXTENDED_ALIGNMENT
+#define _PYOPENCL_COMPLEX_ALIGNMENT(TYPE) 2*sizeof(TYPE)
+#else
+#define _PYOPENCL_COMPLEX_ALIGNMENT(TYPE) sizeof(TYPE)
+#endif
+
 #define PYOPENCL_DECLARE_COMPLEX_TYPE(BASE, BASE_3LTR) \
   typedef union \
   { \
-    struct { BASE x, y; }; \
-    struct { BASE real, imag; }; \
+    struct { BASE x, y; } \
+      __attribute__ ((aligned (_PYOPENCL_COMPLEX_ALIGNMENT(BASE)))); \
+    struct { BASE real, imag; } \
+      __attribute__ ((aligned (_PYOPENCL_COMPLEX_ALIGNMENT(BASE)))); \
   } c##BASE##_t; \
   \
   PYOPENCL_DECLARE_COMPLEX_TYPE_INT(BASE, BASE_3LTR, c##BASE, c##BASE##_t)
