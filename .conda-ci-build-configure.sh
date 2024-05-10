e41aa50f5b66414e98c40cc85ba84e372af54fc8
Matthias Diener <mdiener@illinois.edu>
2024-05-10 11:05:42 -0500

    specify config in pyproject.toml

--- .conda-ci-build-configure.sh
+++
@@ -1,6 +0,0 @@
-case $(uname) in
-  MINGW*|MSYS*) python ./configure.py --cl-inc-dir="$CONDA_PREFIX/Library/include" \
-	  --cl-lib-dir="$CONDA_PREFIX/Library/lib";;
-  *) python ./configure.py --cl-inc-dir="$CONDA_PREFIX/include" \
-	  --cl-lib-dir="$CONDA_PREFIX/lib";;
-esac
--- configure.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python3
-
-from aksetup_helper import configure_frontend
-
-
-configure_frontend()
