f697e3286516136581847300d1e2092980279a7b
Andreas Kloeckner <inform@tiker.net>
2024-06-05 16:49:48 -0500

    Placate mypy about version.py

--- pyopencl/version.py
+++ pyopencl/version.py
@@ -4,5 +4,6 @@ from importlib import metadata
 
 VERSION_TEXT = metadata.version("pyopencl")
 _match = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+assert _match is not None
 VERSION_STATUS = _match.group(2)
 VERSION = tuple(int(nr) for nr in _match.group(1).split("."))
