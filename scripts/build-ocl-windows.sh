137ddb190121098d2b16ff1d21e146ef883e348c
Alexandru Fikl <alexfikl@gmail.com>
2026-06-28 09:56:34 +0300

    feat: bump minimum cmake version

--- scripts/build-ocl-windows.sh
+++ scripts/build-ocl-windows.sh
@@ -8,14 +8,14 @@ git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-ICD-Loader
 git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-Headers
 
 
-cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build
 cmake --build ./OpenCL-Headers/build --target install
 
 # if someone would like to try to create win32 wheels the below lines may be useful
-# cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -DOPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build
+# cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -DOPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build
 # cmake --build ./OpenCL-ICD-Loader/build --target install --config Release
 
-cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -D OPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build2 -A x64
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -D OPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build2 -A x64
 cmake --build ./OpenCL-ICD-Loader/build2 --target install --config Release
 
 echo "PyOpenCL wheel includes Khronos Group OpenCL-ICD-Loader which is licensed as below:" >> ${SCRIPT_DIR}/../LICENSE
